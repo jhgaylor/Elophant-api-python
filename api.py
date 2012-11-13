2533e3fd1806e7f54f49610f347628968238e0ec
Jake Gaylor <jhgaylor@gmail.com>
2012-11-13 07:53:04 -0600

    Update api.py

--- api.py
+++ api.py
@@ -63,13 +63,13 @@ class api():
 
 
 #usage examples
-a = api(key="ormTSJxcEMPj9kJA0p3B")
+#a = api(key="###")
 # print a.champions()
 # print a.items()
 # print a.get_player_stats('76833', 'euw')
 #print a.get_team_by_id('TEAM-e4936d7b-b80e-4367-a76c-5ccf7388c995', 'na')
 #print a.get_team_by_tag_or_name('tsm', 'na')
-print a.get_summoner_names(['76833'])
+#print a.get_summoner_names(['76833'])
 #print a.get_summoner_team_info('mercenaryRPG', 'na')
 
 #need to install slumber, requests
